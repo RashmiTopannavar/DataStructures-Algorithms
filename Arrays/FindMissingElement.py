d46996d802bd571a0782536dad7057e7fddd2b91
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-06-04 16:41:54 -0500

    FindMissingElement_SumApproach implemented

--- Arrays/FindMissingElement.py
+++ Arrays/FindMissingElement.py
@@ -8,7 +8,7 @@ def FindMissingElement(a):
     return -1    
 
 
-a = [1,2,3,4,5,7,8]
+a = [1,2,3,4,5,6,7,8]
 result = FindMissingElement(a) 
 if result != -1:
     print("Missing element is ", result)
@@ -16,6 +16,30 @@ if result != -1:
 else:
     print("No missing values found.")    
 
+
+#######################################################################################################################################
+
+def FindMissingElement_SumApproach(a):
+    sum = 0
+    a_len = len(a) + 1  # +1 cuz 1 element in the array is missing
+    print("length of array", a_len)
+
+    for i in range(len(a)):
+        sum = sum + a[i]
+
+    s_formula = a_len * (a_len+1)//2     #[n*(n+1)]/2
+    print("formula value ",s_formula)
+
+    missingElement = s_formula - sum  
+
+    return missingElement
+
+    
+
+
+a = [1,2,3,4,5,6,7,8,9]
+result = FindMissingElement_SumApproach(a)
+print(result)
 
 
 
