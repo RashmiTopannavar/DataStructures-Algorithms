b64b010b1c207498b023d37688ab754d6e4e65ef
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-06-04 21:12:50 -0500

    findMissing_Hash using HASH

--- Arrays/FindMultipleMissing.py
+++ Arrays/FindMultipleMissing.py
@@ -11,13 +11,39 @@ def findMultipleMissing(a):
             
     
 
-a = [6,7,8,9,11,12,15,16,17,18,19]
-# a = [6,7,8,9]
-res = findMultipleMissing(a)
+# a = [6,7,8,9,11,12,15,16,17,18,19]
+# # a = [6,7,8,9]
+# res = findMultipleMissing(a)
+# if res == []:
+#     print("No missing element")
+# else:
+#     print(res)
+
+
+def findMissing_Hash(a):
+    dict_Hash = {}
+    b = []
+    low =  min(a)
+    high = max(a)
+
+    for i in a:
+        dict_Hash[i] = 1
+
+    for i in range(low, high):
+        if i not in dict_Hash:
+            b.append(i)
+
+    return b
+
+a = [2,6,7,8,9,11,12,15,16,17,18,19]
+res = findMissing_Hash(a)
 if res == []:
     print("No missing element")
 else:
     print(res)
+
+            
+
 
 
                 
