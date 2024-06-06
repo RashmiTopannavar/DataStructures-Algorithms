3ef85fee4a114bead1739d35fcc232f09cc029de
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-06-06 03:22:08 -0500

    countWords.py

---
+++ Strings/countWords.py
@@ -0,0 +1,15 @@
+def countWords(a):
+    # count = 0
+    # b = a.split(' ')
+    # # for i in b:
+    # #     count += 1
+    # # return count   
+    # return len(b) 
+    return len(a.split(' '))
+
+a = "How are you"
+res = countWords(a)
+print(res)
+
+
+
