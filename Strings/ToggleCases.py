69ac7acde9d652b3f50e4c3669189c08463d8e01
Rashmi Venkatesh Topannavar <rashmi.topannavar@gmail.com>
2024-06-06 02:59:13 -0500

    toggleCases implemented

---
+++ Strings/ToggleCases.py
@@ -0,0 +1,18 @@
+def toggleCases(a):
+    vcount = 0
+    ccount = 0
+
+    for i in range(len(a)):
+        if a[i] in 'aeiouAEIOU':
+            vcount += 1
+         
+    
+        elif (ord(a[i]) >= 97 and ord(a[i]) <= 122) or (ord(a[i]) >= 65 and ord(a[i]) <= 90):
+            ccount+= 1 
+    return ccount, vcount
+        
+a = "Hello world"
+consonents, vowels = toggleCases(a)
+print("consonents: ", consonents, "vowels: ", vowels)
+
+
